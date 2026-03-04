30e7353ad9018d6cd848be9553d44508b540e50d
ACG22333 <167335918+ACG22333@users.noreply.github.com>
2026-03-04 18:00:12 +0800

    Update model.go

--- config/model.go
+++ config/model.go
@@ -2,15 +2,43 @@ package config
 
 var ModelReverseMap = map[string]string{}
 var ModelMap = map[string]string{
-	"claude-4-6-sonnet":       "claude46sonnet",
-	"claude-4.6-sonnet-think": "claude46sonnetthinking",
 	"gemini-3.1-pro":          "gemini31pro_high",
 	"gpt-5.2":                 "gpt52",
 	"gpt-5.2-think":           "gpt52_thinking",
+	"claude-4-5-haiku":        "claude45haiku",
+	"claude-4-sonnet":         "claude2",
+	"claude-4-sonnet-think":   "claude37sonnetthinking",
+	"claude-4-5-sonnet":       "claude45sonnet",
+	"claude-4.5-sonnet-think": "claude45sonnetthinking",
+	"claude-4-6-sonnet":       "claude46sonnet",
+	"claude-4.6-sonnet-think": "claude46sonnetthinking",
+	"gemini-2.5-pro":          "gemini25pro",
+	"gemini-3-pro":            "gemini30pro",
+	"kimi-k2.5-thinking":        "kimik25thinking",
+	"grok":                    "grok",
+	"grok-4":                  "grok4",
+	"grok-4-non-thinking":     "grok4nonthinking",
+    "grok-4.1-reasoning":      "grok41reasoning",
+	"grok-4.1-non-reasoning":  "grok41nonreasoning",
+	"o3-pro":                  "o3pro",
+	"gpt-4o":                  "gpt4o",
+	"gpt-4.1":                 "gpt41",
+	"gpt-5.1":                 "gpt51",
+	"gpt-5-think":             "gpt5_thinking",
+	"alpha":                   "pplx_alpha",
+	"beta":                    "pplx_beta",
+	"study":                   "pplx_study",
+	"r1":                      "r1",
 }
 var MaxModelMap = map[string]string{
-	"claude-4.6-opus":       "claude46opus",
-	"claude-4.6-opus-think": "claude46opusthinking",
+	"claude-4-opus":           "claude40opus",
+	"claude-4-opus-think":     "claude40opusthinking",
+	"claude-4.1-opus":         "claude41opus",
+	"claude-4.1-opus-think":   "claude41opusthinking",
+	"claude-4.5-opus":         "claude45opus",
+	"claude-4.5-opus-think":   "claude45opusthinking",
+	"claude-4.6-opus":         "claude46opus",
+	"claude-4.6-opus-think":   "claude46opusthinking",
 }
 
 // Get returns the value for the given key from the ModelMap.
@@ -68,3 +96,4 @@ func buildResponseModels() {
 		})
 	}
 }
+
